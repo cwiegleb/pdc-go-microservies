5e5e4f2264cb38104bfab51270eafeff5433242d
Christian Wiegleb <christian.wiegleb@digitalunit.berlin>
2018-03-20 22:16:58 +0100

    added docker push command to build chain and fixed bug in dealer invoice download

--- dockerize_apps.sh
+++ dockerize_apps.sh
@@ -2,22 +2,27 @@
 
 pushd pdc-cashbox-service
  docker build -t cwiegleb/pdc-cashbox-service:no-article .
+ docker push cwiegleb/pdc-cashbox-service:no-article
 popd
 
 pushd pdc-dealer-service
  docker build -t cwiegleb/pdc-dealer-service:no-article .
+ docker push cwiegleb/pdc-dealer-service:no-article
 popd
 
 pushd pdc-order-service
  docker build -t cwiegleb/pdc-order-service:no-article .
+ docker push cwiegleb/pdc-order-service:no-article
 popd 
 
 pushd pdc-csv-upload-service
  docker build -t cwiegleb/pdc-csv-upload-service:no-article .
+ docker push cwiegleb/pdc-csv-upload-service:no-article
 popd 
 
 pushd pdc-db
  docker build -t cwiegleb/pdc-db:no-article .
+ docker push cwiegleb/pdc-db:no-article
 popd
 
 
